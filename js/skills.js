bb9c1be2bdaae30189432578bc49ded8ac377b54
Kwame Okai Addo <48158461+Kwame-Addo@users.noreply.github.com>
2023-01-09 11:49:28 +0000

    Add files via upload

--- js/skills.js
+++ js/skills.js
@@ -1,5 +1,6 @@
 const expertiseCols = document.querySelectorAll('.expertise-cols1, .expertise-cols2');
 
+
 function handleVisibility(entries) {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
@@ -14,4 +15,26 @@ const observer = new IntersectionObserver(handleVisibility, {
 
 expertiseCols.forEach(col => {
   observer.observe(col);
+});
+
+const topContainer = document.querySelector('.top-container');
+const backToTop = document.querySelector('.back-to-top');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > topContainer.offsetHeight) {
+    backToTop.style.display = 'block';
+  } else {
+    backToTop.style.display = 'none';
+  }
+});
+
+backToTop.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
+$('.back-to-top').on('click', function(event) {
+  event.preventDefault();
+  $('html, body').animate({scrollTop: 0}, 500);
 });
